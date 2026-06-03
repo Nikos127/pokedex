63b60a4d03f95d010b0637cf636bc7ba28d57625
Nikos127 <nikos127@gmail.com>
2026-06-03 10:09:08 +0200

    transfering the templates

--- script.js
+++ script.js
@@ -165,45 +165,6 @@ function getTypeIconUrl(typeName) {
     return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-ix/scarlet-violet/small/${typeId}.png`;
 }
 
-function createDialogTypeHtml(character) {
-    let html = '';
-    for (let i = 0; i < character.types.length; i++) {
-        const typeName = character.types[i].type.name;
-        html += `<span><img src="${getTypeIconUrl(typeName)}" alt="${typeName}" class="type-icon"> ${typeName}</span>`;
-    }
-    return html;
-}
-
-function createDialogStatsHtml(character) {
-    let html = '';
-    for (let i = 0; i < character.stats.length; i++) {
-        const statName = character.stats[i].stat.name;
-        const statValue = character.stats[i].base_stat;
-        html += `
-            <div class="dialog-stat-row">
-                <span>${statName}</span>
-                <span>${statValue}</span>
-            </div>
-        `;
-    }
-    return html;
-}
-
-function createDialogTemplate(character) {
-    const firstType = character.types[0]?.type.name || 'normal';
-    return `
-         <div class="dialog-card ${firstType}">
-            <h2>${character.name}</h2>
-            <img src="${character.sprites.other.home.front_default}" alt="${character.name}">
-            <div class="dialog-types">${createDialogTypeHtml(character)}</div>
-            <div class="dialog-stats">${createDialogStatsHtml(character)}</div>
-            <div class="navigationArrows">
-                <button onclick="openDialogByPrevId(${character.id})" style="rotate: 180deg;"><img src="./images/arrow.png" alt=""></button>
-                <button onclick="openDialogByNextId(${character.id})"><img src="./images/arrow.png" alt=""></button>
-            </div>
-    `;
-}
-
 function findCharacterById(characterId) {
     for (let i = 0; i < allCharacters.length; i++) {
         if (allCharacters[i].id === characterId) {
@@ -279,38 +240,10 @@ function displayCharacters(characters, isSearchResult = false) {
     loadMoreButton.disabled = isSearchResult || isLoading || !hasNextPage;
 }
 
-function noResultsHtml() {
-    return `
-        <div class="no-results">
-            <h3>No match in the Pokedex</h3>
-            <p>This Pokemon is hiding really well right now.</p>
-            <span>Try a different name or just the first few letters.</span>
-        </div>
-    `;
-}
-
 function createCardsHtml(characters) {
     let html = '';
     for (let i = 0; i < characters.length; i++) {
         html += createCard(characters[i]);
     }
     return html;
-}
-
-function createCard(character) {
-    const firstType = character.types[0]?.type.name || 'unknown';
-    const secondType = character.types[1]?.type.name || '';
-
-    let typeHtml = `<img src="${getTypeIconUrl(firstType)}" alt="${firstType}" class="type-icon"> ${firstType}`;
-    if (secondType) {
-        typeHtml += ` <img src="${getTypeIconUrl(secondType)}" alt="${secondType}" class="type-icon"> ${secondType}`;
-    }
-
-    return `
-        <div class="card">
-            <div class="${firstType} border">${character.name}</div>
-            <button onclick="openDialogById(${character.id})" class="${firstType}"><img src="${character.sprites.other.home.front_default}" alt="${character.name}"></button>
-            <div class="type">${typeHtml}</div>
-        </div>
-    `;
 }
---
+++ templates.js
@@ -0,0 +1,66 @@
+function createDialogTypeHtml(character) {
+    let html = '';
+    for (let i = 0; i < character.types.length; i++) {
+        const typeName = character.types[i].type.name;
+        html += `<span><img src="${getTypeIconUrl(typeName)}" alt="${typeName}" class="type-icon"> ${typeName}</span>`;
+    }
+    return html;
+}
+
+function createDialogStatsHtml(character) {
+    let html = '';
+    for (let i = 0; i < character.stats.length; i++) {
+        const statName = character.stats[i].stat.name;
+        const statValue = character.stats[i].base_stat;
+        html += `
+            <div class="dialog-stat-row">
+                <span>${statName}</span>
+                <span>${statValue}</span>
+            </div>
+        `;
+    }
+    return html;
+}
+
+function createDialogTemplate(character) {
+    const firstType = character.types[0]?.type.name || 'normal';
+    return `
+         <div class="dialog-card ${firstType}">
+            <h2>${character.name}</h2>
+            <img src="${character.sprites.other.home.front_default}" alt="${character.name}">
+            <div class="dialog-types">${createDialogTypeHtml(character)}</div>
+            <div class="dialog-stats">${createDialogStatsHtml(character)}</div>
+            <div class="navigationArrows">
+                <button onclick="openDialogByPrevId(${character.id})" style="rotate: 180deg;"><img src="./images/arrow.png" alt=""></button>
+                <button onclick="openDialogByNextId(${character.id})"><img src="./images/arrow.png" alt=""></button>
+            </div>
+    `;
+}
+
+function noResultsHtml() {
+    return `
+        <div class="no-results">
+            <h3>No match in the Pokedex</h3>
+            <p>This Pokemon is hiding really well right now.</p>
+            <span>Try a different name or just the first few letters.</span>
+        </div>
+    `;
+}
+
+function createCard(character) {
+    const firstType = character.types[0]?.type.name || 'unknown';
+    const secondType = character.types[1]?.type.name || '';
+
+    let typeHtml = `<img src="${getTypeIconUrl(firstType)}" alt="${firstType}" class="type-icon"> ${firstType}`;
+    if (secondType) {
+        typeHtml += ` <img src="${getTypeIconUrl(secondType)}" alt="${secondType}" class="type-icon"> ${secondType}`;
+    }
+
+    return `
+        <div class="card">
+            <div class="${firstType} border">${character.name}</div>
+            <button onclick="openDialogById(${character.id})" class="${firstType}"><img src="${character.sprites.other.home.front_default}" alt="${character.name}"></button>
+            <div class="type">${typeHtml}</div>
+        </div>
+    `;
+}
